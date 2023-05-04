59164a6af3e943d6f4e2abf0d25bee6f0de960ee
ldh <ldh@ldhui-MacBookPro.local>
2023-05-05 03:12:58 +0900

    feat:set blogs schema

--- database/migrations/2023_05_04_151753_create_blogs_table.php
+++ database/migrations/2023_05_04_151753_create_blogs_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->json('tags');
+            $table->text('content');
             $table->timestamps();
         });
     }
